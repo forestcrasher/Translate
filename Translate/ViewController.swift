542f4314a819a5e2786cd493fc39713ffa10dba8
forestcrasher <forestcrasher@outlook.com>
2020-09-05 18:16:41 +0300

    Create project

---
+++ Translate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Translate
+//
+//  Created by Anton Pryakhin on 05.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
